850cf04af6accfd923f7330839d24233c38451a1
weerza123zatereed <wee.sma55@gmail.com>
2024-02-04 21:59:17 +0700

    Create Exercise4_Kittikawin_P.py

---
+++ Exercise4_Kittikawin_P.py
@@ -0,0 +1,9 @@
+FE=80.00
+GB=79.00
+ICS=95.60
+CP=69.8
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
