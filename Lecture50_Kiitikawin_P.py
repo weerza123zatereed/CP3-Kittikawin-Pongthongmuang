f2af94ba3e69fa0e7dac95a44c12c2f95a7c01fc
weerza123zatereed <wee.sma55@gmail.com>
2024-03-29 20:26:54 +0700

    Create Lecture50_Kiitikawin_P.py

---
+++ Lecture50_Kiitikawin_P.py
@@ -0,0 +1,13 @@
+def AddNumber(x,y):
+    print(x+y)
+def SubTractNumber(x,y):
+    print(x-y)
+def MultiNumber(x,y):
+    print(x*y)
+def DivideNumber(x,y):
+    print(x/y)
+
+AddNumber(10,25)
+SubTractNumber(10,25)
+MultiNumber(10,25)
+DivideNumber(10,25)
